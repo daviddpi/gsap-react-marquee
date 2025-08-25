c142f88ed5fb7f01c4b794dc0ad72d6acefb3019
daviddpi <piscopodavid97@gmail.com>
2025-08-25 16:55:56 +0200

    fix(exports): enable default import syntax for component

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { default as GSAPReactMarquee } from "./components/gsap-react-marquee";
+export { default } from "./components/gsap-react-marquee";
 export type { GSAPReactMarqueeProps } from "./components/gsap-react-marquee.type";
 export * from "./components/gsap-reactmarquee.utils";
